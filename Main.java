877436e559253f390530592998a6d89c16e9abe4
Sinho Chewi <chewisinho@berkeley.edu>
2015-10-10 16:13:46 -0700

    Changed Main.

---
+++ Main.java
@@ -0,0 +1,14 @@
+package calhacks;
+
+public class Main {
+	
+	private static final int WINDOW_WIDTH = 1280;
+	private static final int WINDOW_HEIGHT = 720; 
+	
+    public static void main(String[] args) {
+        GUI _display = new GUI(args, WINDOW_WIDTH, WINDOW_HEIGHT);
+        Schedule _schedule = new Schedule();
+        TextDisplay _textDisplay = new TextDisplay(_schedule);
+        _textDisplay.run();
+    }
+}
