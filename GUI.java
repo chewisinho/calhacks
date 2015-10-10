8b2b6e3b4d5c33a065836ef2423d8eefaf58a3d2
Chris Pence <6cpence@gmail.com>
2015-10-10 09:43:38 -0700

    Added constructor to GUI class

--- GUI.java
+++ GUI.java
@@ -1,9 +1,30 @@
 import objectdraw.*;
+import Acme.*;
 
 /**
  * GUI class for four year plan
  */
 public class GUI extends WindowController {
+   
+   private static int WIDTH;
+   private static int HEIGHT;
+
+   /**
+    * Constructor for a GUI
+    * @param args command line arguments; pass them in from main
+    * @param width width of the window
+    * @param height height of the window
+    */
+   public GUI(String[] args, int width, int height) {
+      new Acme.MainFrame(this, args, width, height);
+      WIDTH = width;
+      HEIGHT = height;
+   }
+
+   /**
+    * Runs on click of the mouse inside the GUI
+    * @param point point in the windows at which the mouse clicked
+    */
 	public void onMouseClick(Location point) {
       System.out.println("YOOOOOO");
    }
