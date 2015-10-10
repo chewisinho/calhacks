c0b489bda96703c2ab6f009511e88fb01967408f
Chris Pence <6cpence@gmail.com>
2015-10-10 00:52:48 -0700

    Added .jars for GUI

--- GUI.java
+++ GUI.java
@@ -1,3 +1,11 @@
-public class GUI {
-	
+import objectdraw.*;
+
+/**
+ * GUI class for four year plan
+ */
+public class GUI extends WindowController {
+	public void onMouseClick(Location point) {
+      System.out.println("YOOOOOO");
+   }
+   
 }
